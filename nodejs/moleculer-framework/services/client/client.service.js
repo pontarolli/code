4fe6ad839bf7ca68fa4ecc58da72e305586c0990
Ricardo Pasquati <ricardo.pasquati@gmail.com>
2021-03-24 02:46:06 -0300

    Add nodejs folder

---
+++ nodejs/moleculer-framework/services/client/client.service.js
@@ -0,0 +1,30 @@
+const { ServiceBroker } = require("moleculer");
+
+// Create a ServiceBroker
+const broker = new ServiceBroker({
+    transporter: "nats://localhost:4222"
+});
+
+// Define a service
+broker.createService({
+    name: "client",
+});
+
+// Start the broker
+broker.start()
+    // Call the service
+    .then(() => broker.call("math.add", { a: 5, b: 3 }))
+    // Print the response
+    .then(res => console.log("5 + 3 =", res))
+    .catch(err => console.error(`Error occured! ${err.message}`))
+    
+    // Call the service
+    //.then(() => broker.call("greeter.hello"))
+    //.then(() => broker.call("greeter.hello", null, { nodeID: "node-3" }))
+    // Print the response
+    //.then(res => console.log("response =", res))
+    //.catch(err => console.error(`Error occured! ${err.message}`))
+    
+    // Switch to REPL mode
+    //.then(() => {broker.repl();});
+ 
