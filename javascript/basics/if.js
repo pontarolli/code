9323d551b42091a6a5dd873b97da86fa24b504fb
Ricardo Pasquati <ricardo.pasquati@gmail.com>
2021-04-09 13:30:14 -0300

    basic if.js example

---
+++ javascript/basics/if.js
@@ -0,0 +1,16 @@
+if(0){ // if(false) which will force the else path.
+    console.log('if path')
+}
+else{
+    console.log('else path')
+}
+
+
+
+
+if(1){ // if(true) which will force the if path.
+    console.log('if path')
+}
+else{
+    console.log('else path')
+}
