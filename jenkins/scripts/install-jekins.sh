2003eb82d70f214b1150f26d7aaad560409f03b6
Ricardo Pasquati <ricardo.pasquati@gmail.com>
2021-08-09 08:22:25 -0300

    .

---
+++ jenkins/scripts/install-jekins.sh
@@ -0,0 +1,8 @@
+# run jenkins
+mkdir -p /var/jenkins-home
+chown -R 1000:1000 /var/jenkins-home/
+docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins-home:/var/jenkins-home -d --name jenkins jenkins/jenkins:lts
+
+# show endpoint
+echo 'Jenkins installed'
+echo 'You should now be able to access jenkins at: http://'$(curl -s ifconfig.co)':8080'
